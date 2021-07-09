a9434d0fbe6115119c6d1a08451f43753dc3a768
peternguyen39 <54886011+peternguyen39@users.noreply.github.com>
2021-07-09 19:53:28 +0700

    Added placeholder class Reminder & Task

---
+++ app/src/main/java/com/example/remindercalendar/Task.java
@@ -0,0 +1,11 @@
+package com.example.remindercalendar;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Task {
+    public String title;
+    public Date due_date;
+    public String task_description;
+    public ArrayList<Reminder> reminder_list;
+}
