ad78d9a267b382bbe0917e94a278af9409656205
linhlrx <64944040+linhlrx@users.noreply.github.com>
2021-07-13 21:44:47 +0700

    addTask update

--- app/src/main/java/com/example/remindercalendar/AddTaskActivity.java
+++ app/src/main/java/com/example/remindercalendar/AddTaskActivity.java
@@ -1,7 +1,10 @@
 package com.example.remindercalendar;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
@@ -9,28 +12,58 @@ import androidx.fragment.app.DialogFragment;
 public class AddTaskActivity extends AppCompatActivity {
 
     public Task task;
-    private int yy, mm, dd;
+    private int yy, mm, dd, hourOfDay, minute;
+    public TextView dateTextView;
+    public TextView timeTextView;
+    public CheckBox addTask_isStarred;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+        dateTextView = findViewById(R.id.addTask_date_textview);
+        timeTextView = findViewById(R.id.addTask_time_textview);
+        addTask_isStarred = findViewById(R.id.addTask_isStarred_checkbox);
     }
-
 
     public void showDatePicker(View view) {
         DialogFragment newFragment = new com.example.remindercalendar.DatePickerFragment();
         newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
+    public void showTimePicker(View view) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
     public void processDatePickerResult(int year, int month, int day) {
         yy = year;
         mm = month;
         dd = day;
-        task.due_time.set(year, month, day);
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
+        dateTextView.setText(dateMessage);
     }
 
     public void processTimePickerResult(int hourOfDay, int minute) {
+        String hour_string = Integer.toString(hourOfDay);
+        String minute_string = Integer.toString(minute);
+        String timeMessage = (hour_string + ":" + minute_string);
+        timeTextView.setText(timeMessage);
+    }
+
+    @SuppressLint("ResourceType")
+    public void saveButton(View view) {
+        task.title = getString(R.id.addTask_title_edittext);
         task.due_time.set(yy, mm, dd, hourOfDay, minute);
+        task.task_description = getString(R.id.addTask_description_edittext);
+        task.starred = addTask_isStarred.isChecked();
+        finish();
+    }
+
+    public void cancelButton(View view) {
+        finish();
     }
 }
--- app/src/main/java/com/example/remindercalendar/TimePickerFragment.java
+++ app/src/main/java/com/example/remindercalendar/TimePickerFragment.java
@@ -14,13 +14,6 @@ import java.util.Calendar;
 public class TimePickerFragment extends DialogFragment
         implements TimePickerDialog.OnTimeSetListener {
 
-
-    /**
-     * Creates the time picker dialog with the current time from Calendar.
-     *
-     * @param savedInstanceState Saved instance state bundle.
-     * @return TimePickerDialog     The time picker dialog.
-     */
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final Calendar c = Calendar.getInstance();
@@ -32,15 +25,6 @@ public class TimePickerFragment extends DialogFragment
                 DateFormat.is24HourFormat(getActivity()));
     }
 
-
-    /**
-     * Grabs the time and converts it to a string to pass
-     * to the Main Activity in order to show it with processTimePickerResult().
-     *
-     * @param timePicker The time picker view
-     * @param hourOfDay  The hour chosen
-     * @param minute     The minute chosen
-     */
     @Override
     public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
         AddTaskActivity activity = (AddTaskActivity) getActivity();
