2e7523f7a16d1afb60276137b4bd99d57d16af8d
Justin DT <56988254+justindtran2001@users.noreply.github.com>
2021-07-13 19:44:49 +0700

    Add nav drawer

--- app/src/main/java/com/example/remindercalendar/MainActivity.java
+++ app/src/main/java/com/example/remindercalendar/MainActivity.java
@@ -1,31 +1,64 @@
 package com.example.remindercalendar;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ListView;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public TodayListAdapter todayListAdapter;
     private FloatingActionButton fab;
     private RecyclerView todayView;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ListView menuListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        todayView = findViewById(R.id.today_view);
-        //fab.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View v) {
-        //        return;
-        //    }
-        //});
+
+        init();
+        actionToolbar();
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void actionToolbar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.ic_launcher_foreground);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    private void init() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        todayView = (RecyclerView) findViewById(R.id.today_view);
+        menuListView = (ListView) findViewById(R.id.menu_listview);
+        fab = (FloatingActionButton) findViewById(R.id.floating_action_button);
     }
 
     public void showDatePicker(View view) {
